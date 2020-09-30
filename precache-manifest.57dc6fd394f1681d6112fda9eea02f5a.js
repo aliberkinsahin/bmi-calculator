1a50824e5806f7db1c04d285ab2a0bea51b6d0ab
Ali Berkin Şahin <aliberkinsahin@gmail.com>
2020-09-30 18:57:46 +1000

    Updates

---
+++ precache-manifest.57dc6fd394f1681d6112fda9eea02f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49b922d51c9c62109823d9de608357e",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "8eb36bb4e443304cdd56",
+    "url": "/bmi-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c06ab89aa3d93374100",
+    "url": "/bmi-calculator/static/js/2.e9964c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmi-calculator/static/js/2.e9964c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb36bb4e443304cdd56",
+    "url": "/bmi-calculator/static/js/main.747de17c.chunk.js"
+  },
+  {
+    "revision": "68a23b62d065f8673462",
+    "url": "/bmi-calculator/static/js/runtime-main.c5443814.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bmi-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
